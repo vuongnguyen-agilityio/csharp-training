12d8c3e0922460325a057cf47c3fbded8c763864
Vuong Nguyen <vuong.nguyen@asnet.com.vn>
2023-09-27 17:11:27 +0700

    Remove unused code

--- reading/DotNet/samples/xml.cs
+++ reading/DotNet/samples/xml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlTypes;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 using System.Xml;
 
 namespace DotNet.samples
