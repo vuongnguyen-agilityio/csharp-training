95a1285438118f2be54f4a490bf9f800c03fdd34
Vuong Surface <vuongnguyen.dev@gmail.com>
2023-10-10 17:46:31 +0700

    Add assembly sample

---
+++ reading/DotNet/samples/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: AssemblyTitle("My Test Assembly")]
+[assembly: AssemblyDescription("An example assembly")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AgilityIO")]
+[assembly: AssemblyProduct("MyTestProduct")]
+[assembly: AssemblyCopyright("Copyright © AgilityIO")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("aed4613a-1aee-4ae1-af35-d0dc97c92d16")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
