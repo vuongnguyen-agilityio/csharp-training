86a1611c2fdda9d6dd45af9ef8dd9689f1ce64c0
Vuong Nguyen <vuong.nguyen@asnet.com.vn>
2023-10-18 14:26:14 +0700

    Add sample web api with ASP.NET Core Controller

---
+++ dotnet/samples/mslearn-web-api-aspnet-core/mslearn-web-api-aspnet-core/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace mslearn_web_api_aspnet_core.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
