698de8712fc8d743bf38b06110b5915d6e3bbbdb
Vuong Nguyen <vuong.nguyen@asnet.com.vn>
2023-10-26 17:13:51 +0700

    Add Web API with MongoDB (Todo APIs)

---
+++ dotnet/samples/CoreWebAPIs/TodoApi/Models/TodoTask.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TodoApi.Models
+{
+    public class TodoTask
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public decimal Priority { get; set; }
+
+        public string Status { get; set; } = null!;
+    }
+}
---
+++ dotnet/samples/CoreWebAPIs/TodoApi/Services/TodoTasksService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using TodoApi.Models;
+
+namespace TodoApi.Services
+{
+    public class TodoTasksService
+    {
+        private readonly IMongoCollection<TodoTask> _todoTasksCollection;
+
+        public TodoTasksService(
+            IOptions<TodoDatabaseSettings> TodoDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                TodoDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                TodoDatabaseSettings.Value.DatabaseName);
+
+            _todoTasksCollection = mongoDatabase.GetCollection<TodoTask>(
+                TodoDatabaseSettings.Value.TasksCollectionName);
+        }
+
+        public async Task<List<TodoTask>> GetAsync() =>
+            await _todoTasksCollection.Find(_ => true).ToListAsync();
+
+        public async Task<TodoTask?> GetAsync(string id) =>
+            await _todoTasksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(TodoTask newTodoTask) =>
+            await _todoTasksCollection.InsertOneAsync(newTodoTask);
+
+        public async Task UpdateAsync(string id, TodoTask updatedTodoTask) =>
+            await _todoTasksCollection.ReplaceOneAsync(x => x.Id == id, updatedTodoTask);
+
+        public async Task RemoveAsync(string id) =>
+            await _todoTasksCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
