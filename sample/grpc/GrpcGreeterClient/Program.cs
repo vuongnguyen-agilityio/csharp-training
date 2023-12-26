720cf1eb93103ad2c8315540d96d94a0fe559770
VuongNguyen <vuong.nguyen@asnet.com.vn>
2023-12-26 09:28:55 +0700

    Add missing proj

---
+++ sample/grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// The port number must match the port of the gRPC server.
+// The HTTPS port number specified in Properties/launchSettings.json within the GrpcGreeter service project
+using var channel = GrpcChannel.ForAddress("https://localhost:7288");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
