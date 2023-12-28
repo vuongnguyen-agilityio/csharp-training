f912a363514e4e1486636ae1f35b3c5d80acf73c
Vuong Nguyen <vuong.nguyen@asnet.com.vn>
2023-12-28 17:36:38 +0700

    Add grpc samples

--- sample/grpc/GrpcGreeter/Program.cs
+++ sample/grpc/GrpcGreeter/Program.cs
@@ -3,13 +3,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc(options =>
+{
+    // Configure for all services 
+    options.EnableDetailedErrors = true;
+    options.MaxReceiveMessageSize = 2 * 1024 * 1024; // 2 MB
+    options.MaxSendMessageSize = 5 * 1024 * 1024; // 5 MB
+}).AddServiceOptions<GreeterService>(options =>
+{
+    // Configure for specify service
+    options.MaxReceiveMessageSize = 2 * 1024 * 1024; // 2 MB
+    options.MaxSendMessageSize = 5 * 1024 * 1024; // 5 MB
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
 
